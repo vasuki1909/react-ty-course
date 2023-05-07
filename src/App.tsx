eef69c9ec54f57c4014a6c8247c8eaea440f2ea7
Vasuki Rohilla <vasuki.rohilla@thoughtworks.com>
2023-05-07 21:24:04 +0530

    refactor app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import GuestList from './state/GuestList';
+import UserSearch from './state/UserSearch';
 
 function App() {
   return (
-    <div className="App">
-          Hii!! I am started
-    </div>
+    <UserSearch/>
+      //  <GuestList/> 
   );
 }
 
